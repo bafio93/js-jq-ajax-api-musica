60c18ddd2a35c93473c9f15e5f1d35d541fd212d
Previtali_Fabio <fabprev93@gmail.com>
2019-12-13 17:10:16 +0100

    Finito! (Bonus compreso!) :D

--- main.js
+++ main.js
@@ -15,12 +15,29 @@ $(document).ready(function(){
                     "poster":current_selection.poster,
                     "title":current_selection.title,
                     "author":current_selection.author,
-                    "year":current_selection.year
+                    "year":current_selection.year,
+                    "genre":current_selection.genre
                 };
                 // Ingetto le variabili nella funzione template:
                 var html_finale = template_function(variabili);
                 $(".cds-container").append(html_finale);
             }
         }
+    });
+    // BONUS: Interazione con il menù a tendina (select):
+    $(".selettore").change(function(){
+        // Intercetto il nuovo valore del campo:
+        var current_genre = $(this).val();
+        // Ora verifico quali tra tutti i CD hanno questo genere, e li rendo visibili (o meno) grazie ad una classe. -> EACH()
+        // Ma prima, rendiamoli visibili tutti! (Altrimenti è come se stessi facendo un AND tra le due ricerche!)
+        $(".cd").removeClass("non_active");
+        $(".cd").each(function(){
+            // Le due condizioni si possono tradurre:
+            // 1) Il CD che sto ciclando non appartiene al genere corrente.
+            // 2) Ho effettivamente selezionato un genere.
+            if ((!$(this).hasClass(current_genre)) && (current_genre.length>0) ) {
+                $(this).addClass("non_active");
+            }
+        })
     })
 })
